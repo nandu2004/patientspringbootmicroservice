d10e3650492ed5758a1a95ae722654e83fc9dfad
nandu2004 <usnandagopa@gmail.com>
2026-03-08 23:13:24 +0530

    checked sample  grpc on local and on docker

--- billing-service/src/main/java/com/example/billing_service/grpc/BillingGrpcSevice.java
+++ billing-service/src/main/java/com/example/billing_service/grpc/BillingGrpcSevice.java
@@ -1,14 +1,22 @@
 package com.example.billing_service.grpc;
 
+import billing.BillingResponse;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+@Slf4j
 @GrpcService
 public class BillingGrpcSevice extends BillingServiceImplBase {
     @Override
     public void createBillingAccount(billing.BillingRequest billingRequest,
             StreamObserver<billing.BillingResponse> responseObserver) {
+        log.info("createbillingacc request recieved {}", billingRequest.toString());
+        BillingResponse response = BillingResponse.newBuilder().setAccountId("12345").setStatus("ACTIVE").build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
 
     }
 
